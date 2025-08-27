72cd374689c128729a81349c7fbfc5e41cff487a
hexenzeichen <ndrkrpv@gmail.com>
2025-08-27 23:23:54 +0300

    Add the new controller

---
+++ src/Controller/StarshipController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StarshipController extends AbstractController
+{
+    #[Route('/starships/{id<\d+>}')]
+    public function show(int $id): Response
+    {
+        dd($id);
+    }
+}
