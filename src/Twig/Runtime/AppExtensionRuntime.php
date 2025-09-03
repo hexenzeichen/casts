2475bc8e35d20053e0ef60ed1493d03738737e2a
hexenzeichen <ndrkrpv@gmail.com>
2025-09-03 22:39:15 +0300

    Complete the twig extension

--- src/Twig/Runtime/AppExtensionRuntime.php
+++ src/Twig/Runtime/AppExtensionRuntime.php
@@ -2,17 +2,26 @@
 
 namespace App\Twig\Runtime;
 
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Twig\Extension\RuntimeExtensionInterface;
 
 class AppExtensionRuntime implements RuntimeExtensionInterface
 {
-    public function __construct()
+    public function __construct(
+        private HttpClientInterface $client,
+        private CacheInterface $issLocationPool,
+    )
     {
-        // Inject dependencies if needed
+
     }
 
-    public function doSomething($value)
+    public function getIssLocationData()
     {
-        // ...
+        return $this->issLocationPool->get('iss_location_data', function (): array {
+            $response = $this->client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
+
+            return $response->toArray();
+        });
     }
 }
