f65cd1a5e80dbc8001660da412f82009ffcc0a59
hexenzeichen <ndrkrpv@gmail.com>
2025-09-09 09:33:22 +0300

    Update the migration

--- migrations/Version20260313062932.php
+++ migrations/Version20260313062932.php
@@ -16,6 +16,7 @@ final class Version20260313062932 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        $this->addSql('UPDATE starship SET slug = id, created_at = arrived_at, updated_at = arrived_at');
         $this->addSql('ALTER TABLE starship CHANGE slug slug VARCHAR(255) NOT NULL, CHANGE updated_at updated_at DATETIME NOT NULL, CHANGE created_at created_at DATETIME NOT NULL');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_C414E64A989D9B62 ON starship (slug)');
     }
