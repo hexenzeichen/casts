a5c4456cd38b8b1f60005209bee74e3da4d7dd5a
hexenzeichen <ndrkrpv@gmail.com>
2025-09-09 09:27:31 +0300

    Add the migration

---
+++ migrations/Version20260313062548.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260313062548 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add slug and timestamps to starship';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE starship ADD slug VARCHAR(255) DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL, ADD created_at DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE starship DROP slug, DROP updated_at, DROP created_at');
+    }
+}
