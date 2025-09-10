7830c9f87d2251dbffd327a0f09abc789adb6423
hexenzeichen <ndrkrpv@gmail.com>
2025-09-10 09:40:57 +0300

    Simplify the controller

--- src/Controller/StarshipController.php
+++ src/Controller/StarshipController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Repository\StarshipRepository;
+use App\Entity\Starship;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,13 +12,8 @@ use Symfony\Component\Routing\Attribute\Route;
 class StarshipController extends AbstractController
 {
     #[Route('/starships/{id<\d+>}', name: 'app_starship_show')]
-    public function show(int $id, StarshipRepository $repository): Response
+    public function show(Starship $ship): Response
     {
-        $ship = $repository->find($id);
-        if (!$ship) {
-            throw $this->createNotFoundException('Starship not found');
-        }
-
         return $this->render('starship/show.html.twig', [
             'ship' => $ship,
         ]);
