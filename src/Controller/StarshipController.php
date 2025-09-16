9eb71f7b348d5652acc86e3c701ab827688dcd20
hexenzeichen <ndrkrpv@gmail.com>
2025-09-16 21:18:57 +0300

    Add parts menu item, remove iss data

--- src/Controller/StarshipController.php
+++ src/Controller/StarshipController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Starship;
-use App\Repository\StarshipPartRepository;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
