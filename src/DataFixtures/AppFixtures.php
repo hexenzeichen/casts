d97088f19d0414ca889628c7aa5d8ba2d377f34f
hexenzeichen <ndrkrpv@gmail.com>
2025-09-20 09:32:06 +0300

    Update the fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,10 +2,9 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Droid;
 use App\Entity\Starship;
+use App\Entity\StarshipDroid;
 use App\Entity\StarshipPart;
-use App\Entity\StarshipStatusEnum;
 use App\Factory\DroidFactory;
 use App\Factory\StarshipFactory;
 use App\Factory\StarshipPartFactory;
@@ -16,33 +15,6 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $starship = StarshipFactory::createOne([
-            'name' => 'USS LeafyCruiser (NCC-0001)',
-            'class' => 'Garden',
-            'captain' => 'Jean-Luc Pickles',
-            'status' => StarshipStatusEnum::IN_PROGRESS,
-            'arrivedAt' => new \DateTimeImmutable('-1 day'),
-        ]);
-
-        $droid1 = new Droid();
-        $droid1->setName('IHOP-123');
-        $droid1->setPrimaryFunction('Pancake chef');
-        $droid1->addStarship($starship);
-        $manager->persist($droid1);
-
-        $droid2 = new Droid();
-        $droid2->setName('D-3P0');
-        $droid2->setPrimaryFunction('C-3PO\'s voice coach');
-        $droid2->addStarship($starship);
-        $manager->persist($droid2);
-
-        $droid3 = new Droid();
-        $droid3->setName('BONK-5000');
-        $droid3->setPrimaryFunction('Comedy sidekick');
-        $droid3->addStarship($starship);
-        $manager->persist($droid3);
-        $manager->flush();
-
         $starship = new Starship();
         $starship->setName('USS Taco Tuesday');
         $starship->setClass('Tex-Mex');
@@ -59,9 +31,19 @@ class AppFixtures extends Fixture
         $manager->flush();
 
         DroidFactory::createMany(100);
-        StarshipFactory::createMany(20,  fn() => [
-            'droids' => DroidFactory::randomRange(1, 5),
+        StarshipFactory::createMany(20, fn() => [
+//            'droids' => DroidFactory::randomRange(1, 5),
         ]);
         StarshipPartFactory::createMany(50);
+
+        $ship = StarshipFactory::random();
+        $droid = DroidFactory::random();
+
+        $starshipDroid = new StarshipDroid();
+        $starshipDroid->setStaship($ship);
+        $starshipDroid->setDroid($droid);
+        $manager->persist($starshipDroid);
+        $manager->flush();
+
     }
 }
