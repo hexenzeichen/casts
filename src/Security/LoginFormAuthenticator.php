b0e1d2ae8086177c1684810bd29fe243b331abbe
hexenzeichen <ndrkrpv@gmail.com>
2025-09-24 21:33:32 +0300

    Add Passport to authenticate

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,36 +2,48 @@
 
 namespace App\Security;
 
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 
 class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
+
+
     public function supports(Request $request): bool
     {
-        return $request->attributes->get('_route') === 'app_login'
-        && $request->isMethod('POST');
+        return ($request->getPathInfo() === '/login' && $request->isMethod('POST'));
     }
 
     public function authenticate(Request $request): Passport
     {
-        dump($request->request->all());die;
-        // TODO: Implement authenticate() method.
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+
+        return new Passport(
+            new UserBadge($email),
+            new CustomCredentials(function($credentials, User $user) {
+                dd($credentials, $user);
+            }, $password)
+        );
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        // TODO: Implement onAuthenticationSuccess() method.
+
     }
 
     protected function getLoginUrl(Request $request): string
     {
-        // TODO: Implement getLoginUrl() method.
+
     }
 
 }
