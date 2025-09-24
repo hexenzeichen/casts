7d6a5c5b95c9f9029ad219354d82b7bbaa6c3dc7
hexenzeichen <ndrkrpv@gmail.com>
2025-09-24 21:43:54 +0300

    Load user manually

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -3,6 +3,7 @@
 namespace App\Security;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -16,7 +17,11 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 
 class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
-
+    private UserRepository $userRepository;
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
 
     public function supports(Request $request): bool
     {
@@ -29,7 +34,14 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         $password = $request->request->get('password');
 
         return new Passport(
-            new UserBadge($email),
+            new UserBadge($email, function($userIdentifier) {
+                // optionally pass a callback to load the User manually
+                $user = $this->userRepository->findOneBy(['email' => $userIdentifier]);
+                if (!$user) {
+                    throw new UserNotFoundException();
+                }
+                return $user;
+            }),
             new CustomCredentials(function($credentials, User $user) {
                 dd($credentials, $user);
             }, $password)
