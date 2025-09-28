20c08c2c6558ae2ab03067ca20d1498518c1d8af
hexenzeichen <ndrkrpv@gmail.com>
2025-09-28 22:19:26 +0300

    Grant authenticated user access to the part controller

--- src/Controller/PartController.php
+++ src/Controller/PartController.php
@@ -7,10 +7,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class PartController extends AbstractController
 {
     #[Route('/part', name: 'app_part_index')]
+    #[IsGranted('ROLE_USER')]
     public function index(StarshipPartRepository $repository, Request $request): Response
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
